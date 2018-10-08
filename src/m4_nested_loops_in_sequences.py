4997f9394903648dc69027415a170b0d7b735f5f
arock19 <guptaa5@rose-hulman.edu>
2018-10-08 17:08:04 -0400

    0

--- src/m4_nested_loops_in_sequences.py
+++ src/m4_nested_loops_in_sequences.py
@@ -3,8 +3,8 @@ This project demonstrates NESTED LOOPS (i.e., loops within loops)
 in the context of SEQUENCES OF SUB-SEQUENCES.
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Achintya Gupta.
+"""  # TO: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -116,8 +116,11 @@ def multiply_numbers(sequence_of_lists):
        and the elements of the lists can be multiplied by an integer.
        [FYI: This 'can be multiplied ...' is an example of DUCK TYPING.]
     """
+    for k in range(len(sequence_of_lists)):
+        for l in range(len(sequence_of_lists[k])):
+            sequence_of_lists[k][l]=(k+1)*sequence_of_lists[k][l]
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # TO: 2. Implement and test this function.
     #  ** READ THE TESTS that have been written for you (ABOVE).
     #  ** ASK QUESTIONS if you do not understand the TESTS (ABOVE).
     #
@@ -133,7 +136,7 @@ def multiply_numbers(sequence_of_lists):
 def run_test_sum_numbers():
     """ Tests the    sum_numbers    function. """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement this TEST function.
+    # TO: 3. Implement this TEST function.
     #   It TESTS the  sum_numbers  function defined below.
     #   Include at least **   3   ** tests (we wrote two for you).
     # ------------------------------------------------------------------
@@ -153,7 +156,10 @@ def run_test_sum_numbers():
     print('Expected and actual are:', expected, answer)
 
     # TO DO 3 (continued): Add your ADDITIONAL test(s) here:
-
+    # Test 3:
+    expected = 1.25
+    answer = sum_numbers(([200,-200,0.5], [3*0.25], [0]))
+    print('Expected and actual are:', expected, answer)
 
 def sum_numbers(seq_seq):
     """
@@ -165,8 +171,14 @@ def sum_numbers(seq_seq):
     Preconditions:  the given argument is a sequences of sequences,
                     and each item in the subsequences is a number.
     """
+    sm=0
+    for k in range(len(seq_seq)):
+        for l in range(len(seq_seq[k])):
+            sm=sm+seq_seq[k][l]
+    return sm
+
     # ------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # TO: 4. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     # ------------------------------------------------------------------
 
@@ -216,8 +228,11 @@ def print_characters(sequence_of_strings):
        !
     Precondition:  the given argument is a sequence of strings.
     """
+    for k in range(len(sequence_of_strings)):
+        for l in range(len(sequence_of_strings[k])):
+            print('{}'.format(sequence_of_strings[k][l]))
     # ------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # TO: 5. Implement and test this function.
     #  ** READ THE TESTS that have been written for you (ABOVE).
     #  ** ASK QUESTIONS if you do not understand the TESTS (ABOVE).
     # ------------------------------------------------------------------
@@ -269,8 +284,14 @@ def print_characters_slanted(sequence_of_strings):
             !
     Precondition:  the given argument is a sequence of strings.
     """
+    for k in range(len(sequence_of_strings)):
+        for l in range(len(sequence_of_strings[k])):
+            for j in range(l):
+                print(" ",end='')
+            print('{}'.format(sequence_of_strings[k][l]))
+
     # ------------------------------------------------------------------
-    # TODO: 6. Implement and test this function.
+    # TO: 6. Implement and test this function.
     #  ** READ THE TESTS that have been written for you (ABOVE).
     #  ** ASK QUESTIONS if you do not understand the TESTS (ABOVE).
     #
